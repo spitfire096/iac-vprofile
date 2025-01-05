69e524b91bfada4db59fd8a1e2a186b1eb140423
spitfire096 <domeji14@gmail.com>
2025-01-05 00:44:58 -0500

    updated code with corrected s3 region

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprogitops25"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.10.3"
